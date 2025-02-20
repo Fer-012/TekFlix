73febd7b2f82a44d2cd76c2931297a57ab655cd0
Fer-012 <aouachriferdaoues@gmail.com>
2025-02-20 14:47:08 +0100

    angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    MovieListComponent,
+    MovieDetailComponent,
+    FavoritesComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
